efb3bd449bc8c59b69ea70498885565c693e54f2
carlosismael4 <carlosismael4@hotmail.com>
2017-01-03 00:44:07 -0600

    dgdgdfgdfgdfgdfg

--- app/src/main/java/com/example/ismaelcarlos/geolocationtest/UniversityMap.java
+++ app/src/main/java/com/example/ismaelcarlos/geolocationtest/UniversityMap.java
@@ -1,18 +1,37 @@
 package com.example.ismaelcarlos.geolocationtest;
 
-import android.support.v4.app.FragmentActivity;
+
+
+import android.graphics.Point;
 import android.os.Bundle;
+
+
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.Projection;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MapStyleOptions;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class UniversityMap extends FragmentActivity implements OnMapReadyCallback {
+public class UniversityMap extends AppCompatActivity implements OnMapReadyCallback,NavigationView.OnNavigationItemSelectedListener {
 
     private GoogleMap mMap;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,9 +41,31 @@ public class UniversityMap extends FragmentActivity implements OnMapReadyCallbac
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+        // More info: http://codetheory.in/difference-between-setdisplayhomeasupenabled-sethomebuttonenabled-and-setdisplayshowhomeenabled/
+
+        //MENU
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+        //MENU
     }
-
-
     /**
      * Manipulates the map once available.
      * This callback is triggered when the map is ready to be used.
@@ -37,10 +78,111 @@ public class UniversityMap extends FragmentActivity implements OnMapReadyCallbac
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
+        // Add a marker in UAT tamaulipas and move the camera
+        LatLng UAT = new LatLng(23.7157506,-99.1519597);
+        mMap.addMarker(new MarkerOptions().position(UAT).title("4toPiso"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(UAT,17));
+        mMap.setMapStyle(MapStyleOptions.loadRawResourceStyle(this,R.raw.map_style));
+        addFacultary();
+        position();
+    }
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+    public void position(){
+        mMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
+            @Override
+            public void onMapClick(LatLng latLng) {
+                Projection proj = mMap.getProjection();
+                Point coord = proj.toScreenLocation(latLng);
+
+                Toast.makeText(
+                        UniversityMap.this,
+                        "Click\n" +
+                                "Lat: " + latLng.latitude + "\n" +
+                                "Lng: " + latLng.longitude + "\n" +
+                                "X: " + coord.x + " - Y: " + coord.y,
+                        Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
+    public void addFacultary(){
+        mMap.addMarker(new MarkerOptions()
+                .position(new LatLng(23.71640728,-99.1511106))
+                .title("Comercio"));
+        mMap.addMarker(new MarkerOptions()
+                .position(new LatLng(23.7172535726,-99.1512420))
+                .title("CELLAP"));
+        mMap.addMarker(new MarkerOptions()
+                .position(new LatLng(23.7172572571,-99.1521712))
+                .title("Trabajo Social"));
+        mMap.addMarker(new MarkerOptions()
+                .position(new LatLng(23.7171293570,-99.14989806))
+                .title("Ciencias"));
+        mMap.addMarker(new MarkerOptions()
+                .position(new LatLng(23.71822404,-99.15269393))
+                .title("Gimnacio Multi"));
+        mMap.addMarker(new MarkerOptions()
+                .position(new LatLng(23.71552169,-99.15311230))
+                .title("FIC"));
+        mMap.addMarker(new MarkerOptions()
+                .position(new LatLng(23.7162351,-99.1522213))
+                .title("Centro de Excelencia"));
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_uat, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_camera) {
+            // Handle the camera action
+        } else if (id == R.id.nav_gallery) {
+
+        } else if (id == R.id.nav_slideshow) {
+
+        } else if (id == R.id.nav_manage) {
+
+        } else if (id == R.id.nav_share) {
+
+        } else if (id == R.id.nav_send) {
+
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
     }
 }
